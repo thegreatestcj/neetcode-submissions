17fb9364373dc356641b7137b5aa58f2fb96926e
thegreatestcj <143751512+thegreatestcj@users.noreply.github.com>
2026-06-14 20:56:16 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0;
+        int right = s.size() - 1;
+        while (left <= right)
+        {
+            if (isalnum(s[left]) && isalnum(s[right]))
+            {
+                if (tolower(s[left])!= tolower(s[right])) return false;
+                left++;
+                right--;
+            } else if (!isalnum(s[left]))
+            {
+                left++;
+            } else
+            {
+                right--;
+            }
+            
+        }
+        return true;
+    }
+};
